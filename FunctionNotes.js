22b2545c09fb5812b32a14949306f40cddd6e9e0
4waj <4waj@fresnou.org>
2023-03-14 13:58:41 -0700

    initial commit

---
+++ FunctionNotes.js
@@ -0,0 +1,40 @@
+// //function notes
+// //Step 1 - Define the function
+// function sayHello(){
+//     console.log("Hello");
+// }
+// //step 2 - call (use) the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// function printMax() {
+//     let x = 6;
+//     let y = 29;
+//     if (x>y){
+//         console.log(`the larger number is ${x}`)
+//     } else {
+//         console.log(` the larger number is ${y}`)
+//     }
+// }
+//
+// printMax()
+
+// function printMax(x, y) {
+//     if (x>y){
+//         console.log(`the larger number is ${x}`)
+//     } else {
+//         console.log(` the larger number is ${y}`)
+//     }
+// }
+//
+// printMax(8, 3)
+//
+// printMax(7, 9)
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+sum(1, 3)
+sum(1802371, 123124134)
