6fbab1a8e64b5cc79266bce31b1e3c00cbd23a02
4waj <4waj@fresnou.org>
2023-03-14 14:18:50 -0700

    day1

---
+++ F1A.js
@@ -0,0 +1,35 @@
+//question1
+function sum(number1, number2){
+    console.log(`${number1} Length x ${number2} Width, the area is ${number1*number2}`);
+}
+sum(9, 3)
+
+//question2
+function sum(number1, number2){
+    console.log(`${number1} radius x ${number2}, the area is ${number1*number2}`);
+}
+sum(9, 3.14)
+
+//question3
+function printMax(x, y) {
+    if (x>y){
+        console.log(`${x}`)
+    } else {
+        console.log(`${y}`)
+    }
+}
+
+printMax(8, 3)
+
+//question4
+function printMax(x, y) {
+    if (x==y){
+        console.log(`${x} is equal to ${y}`)
+    } else {
+        console.log(`The numbers are not equal`)
+    }
+}
+
+printMax(3, 3)
+
+//question5
---
+++ Functions1.js
@@ -0,0 +1,14 @@
+function sum(number1, number2){
+    console.log(`${number1} Length x ${number2} Width, the area is ${number1*number2}`);
+}
+function sum1(number3, number4){
+    console.log(`${number3} Length x ${number4} Width, the area is ${number3*number4}`);
+    if (sum1 > sum){
+         console.log(`${number3*number4} is larger!`)
+    }
+    else (sum > sum1){
+        console.log(`${number3*number4} is larger!`)
+    }
+}
+sum(9, 3)
+sum1(7, 4)
