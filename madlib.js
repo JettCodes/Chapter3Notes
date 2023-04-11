122a6b3da05311071821eb35f809cdc327fb0d45
4waj <4waj@fresnou.org>
2023-04-11 14:14:40 -0700

    code for words

---
+++ madlib.js
@@ -0,0 +1,26 @@
+const readLine=require('readline-sync');
+
+let word1 = readLine.question("Adjective");
+let word2 = readLine.question("VerbING");
+let word3 = readLine.question("Adjective");
+let word4 = readLine.question("VerbING");
+let word5 = readLine.question("Noun");
+let word6 = readLine.question("Adjective");
+let word7 = readLine.question("Noun");
+let word8 = readLine.question("VerbED");
+let word9 = readLine.question("Adjective");
+let word10 = readLine.question("VerbED");
+let word11 = readLine.question("Noun");
+let word12 = readLine.question("VerbING");
+let word13 = readLine.question("NounPLACE");
+let word14 = readLine.question("NounNmber");
+let word15 = readLine.question("VerbING");
+let word16 = readLine.question("NounPlural");
+let word17 = readLine.question("VerbING");
+let word18 = readLine.question("Verb");
+let word18 = readLine.question("Adjective");
+let word20 = readLine.question("Adjective");
+
+console.log()
+
+
