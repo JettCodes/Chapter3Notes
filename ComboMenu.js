2084b9997c3898e916a9e284e7d242cad30f67c5
4waj <4waj@fresnou.org>
2023-04-24 14:32:25 -0700

    commit12

--- ComboMenu.js
+++ ComboMenu.js
@@ -6,3 +6,14 @@ order.push(  readLine.question("What sandwhich would you like to order? \nWe hav
 
 console.log(`You chose: ${order[0]}`);
 
+
+let drink = []
+
+drink.push(readLine.question("Would you like a drink with this meal? \nWe have small $1.00, medium $1.75, large $2.25!"))
+
+console.log(`You got a ${drink[0]}`)
+
+
+if(answer == "yes"){
+    console.log()
+}
